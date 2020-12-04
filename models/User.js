ec15add06c1606564e04874ad6dcb014e0be3362
Sandeep Ganguly <sandeep@example.com>
2020-12-04 18:20:24 +0530

    added user model

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const momemt = require('moment');
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  profile_pic: {
+    type: String,
+    default: "empty-avatar.jpg"
+  },
+  createdAt: {
+    type: Date,
+    default: momemt().format("DD/MM/YYYY") + ";"+ momemt().format("hh:mm:ss")
+  },
+  updatedAt: {
+    type: Date,
+    default: momemt().format("DD/MM/YYYY") + ";"+ momemt().format("hh:mm:ss")
+  }
+});
+
+// create user model
+mongoose.model("users", userSchema);
+
+// export user model
+module.exports = mongoose.model("users");
