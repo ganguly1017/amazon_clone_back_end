1cf8e20685d1b70ef59beb33f1a7b876ad05c1e7
Sandeep Ganguly <sandeep@example.com>
2020-12-04 19:12:32 +0530

    user register route setup

--- routes/users.js
+++ routes/users.js
@@ -29,4 +29,36 @@ router.get(
 );
 
 
+// user register route
+// Access: public
+// url: http://localhost:500/api/users/register
+
+router.post(
+  '/register',
+  [
+    // check empty fields
+    check('username').not().isEmpty().trim().escape(),
+    check('password').not().isEmpty().trim().escape(),
+
+    // check email
+    check('email').isEmail().normalizeEmail()
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+
+    // check errors is not empty
+    if(!errors.isEmpty()){
+      return res.status(400).json({
+        "status": false,
+        "errors": errors.array()
+      });
+    }
+
+    return res.status(200).json({
+      "status": true,
+      "data": req.body
+    });
+  }
+);
+
 module.exports = router;
